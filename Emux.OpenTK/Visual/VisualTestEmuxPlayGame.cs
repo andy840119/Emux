967429fd46b1ab650cfd3d976fbd96b3c509562e
為什麼 <andy840119@gmail.com>
2018-10-08 23:43:27 +0900

    1. remove WinMmTimer 2. Move DeviceManager to TestCase

--- Emux.OpenTK/Visual/VisualTestEmuxPlayGame.cs
+++ Emux.OpenTK/Visual/VisualTestEmuxPlayGame.cs
@@ -2,41 +2,35 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Emux.GameBoy.Cartridge;
+using Emux.GameBoy.Cheating;
 using Emux.GameBoy.Graphics;
+using Emux.NAudio;
 using Emux.OpenTK.Emux;
+using NAudio.Wave;
 using NUnit.Framework;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Testing;
+using osu.Framework.Timing;
 using OpenTK.Graphics;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using IClock = Emux.GameBoy.Cpu.IClock;
 
 namespace Emux.OpenTK.Visual
 {
     [TestFixture]
     public class VisualTestEmuxPlayGame : TestCase
     {
-        public readonly DeviceManager DeviceManager = new DeviceManager();
-        private GameBoy.GameBoy _currentDevice;
-        private EmuxGameContainer _displayScreen;
-
+        public GameBoyContainer GameBoyContainer;
         public VisualTestEmuxPlayGame()
         {
-            Add(_displayScreen = new EmuxGameContainer
-            {
-                Width = 160 * 4,
-                Height = 144 * 4
-            });
-            InitialTestStep();
-        }
+            Add(GameBoyContainer = new GameBoyContainer());
 
-        void OnDeviceChanged()
-        {
-            _currentDevice = DeviceManager.CurrentDevice;
-            _currentDevice.Gpu.VideoOutput = _displayScreen;
+            InitialTestStep();
         }
 
         void InitialTestStep()
@@ -45,55 +39,233 @@ namespace Emux.OpenTK.Visual
             AddStep("Load game", () =>
             {
                 string romPath = "Resources/ROM/Tetris.gb";
-                DeviceManager.LoadDevice(romPath, Path.ChangeExtension(romPath, ".sav"));
-                OnDeviceChanged();
+                GameBoyContainer.DeviceManager.LoadDevice(romPath, Path.ChangeExtension(romPath, ".sav"));
+                GameBoyContainer.OnDeviceChanged();
             });
 
             //run game
             AddStep("Run", () =>
             {
-                if(_currentDevice != null && !_currentDevice.Cpu.Running)
+                if(GameBoyContainer.CurrentDevice != null && !GameBoyContainer.CurrentDevice.Cpu.Running)
                 {
-                    _currentDevice.Cpu.Run();
+                    GameBoyContainer.CurrentDevice.Cpu.Run();
                 }
             });
 
             //reset and reload game
             AddStep("Reset", () =>
             {
-                _currentDevice.Reset();
+                GameBoyContainer.CurrentDevice.Reset();
             });
 
             //stop game
             AddStep("Step", () =>
             {
-                _currentDevice.Cpu.Step();
+                GameBoyContainer.CurrentDevice.Cpu.Step();
             });
 
             //break game
             AddStep("Break", () =>
             {
-                _currentDevice.Cpu.Break();
+                GameBoyContainer.CurrentDevice.Cpu.Break();
             });
 
             //set breakPoint
             AddStep("Set BreakPoint", () =>
             {
-                _currentDevice.Cpu.Step();
+                GameBoyContainer.CurrentDevice.Cpu.Step();
             });
 
             //clean all breakpoint
             AddStep("Reset BreakPoint", () =>
             {
-                _currentDevice.Cpu.ClearBreakpoints();
+                GameBoyContainer.CurrentDevice.Cpu.ClearBreakpoints();
             });
         }
 
         protected override void Dispose(bool isDisposing)
         {
             //unload device
-            DeviceManager.UnloadDevice();
+            GameBoyContainer.DeviceManager.UnloadDevice();
             base.Dispose(isDisposing);
+        }
+    }
+
+    public class GameBoyContainer : Container
+    {
+        private readonly DeviceManager _deviceManager;
+        private GameBoy.GameBoy _currentDevice;
+        private readonly EmuxGameContainer _displayScreen;
+
+        public GameBoy.GameBoy CurrentDevice => _currentDevice;
+        public DeviceManager DeviceManager => _deviceManager;
+
+        public GameBoyContainer()
+        {
+            Add(_deviceManager = new DeviceManager());
+            Add(_displayScreen = new EmuxGameContainer
+            {
+                Width = 160 * 4,
+                Height = 144 * 4
+            });
+        }
+
+        public void OnDeviceChanged()
+        {
+            _currentDevice = _deviceManager.CurrentDevice;
+            _currentDevice.Gpu.VideoOutput = _displayScreen;
+        }
+    }
+
+    public class DeviceManager : Container
+    {
+        public event EventHandler<DeviceEventArgs> DeviceLoaded;
+        public event EventHandler<DeviceEventArgs> DeviceUnloaded;
+        public event EventHandler DeviceChanged;
+
+        private GameBoy.GameBoy _currentDevice;
+        private StreamedExternalMemory _currentExternalMemory;
+        private readonly GameBoyTicker _ticker;
+
+        public DeviceManager()
+        {
+            Add(_ticker = new GameBoyTicker());
+            AudioMixer = new GameBoyNAudioMixer();
+            var player = new DirectSoundOut();
+            player.Init(AudioMixer);
+            player.Play();
+
+            GamesharkController = new GamesharkController();
+            //Properties.Settings.Default.PropertyChanged += SettingsOnPropertyChanged;//andy840119 : 
+
+            Breakpoints = new Dictionary<ushort, BreakpointInfo>();
+        }
+
+        public GameBoyNAudioMixer AudioMixer
+        {
+            get;
+        }
+
+        public GameBoy.GameBoy CurrentDevice
+        {
+            get { return _currentDevice; }
+            private set
+            {
+                if (_currentDevice != value)
+                {
+                    _currentDevice = value;
+                    if (value != null)
+                    {
+                        AudioMixer.Connect(value.Spu);
+                        GamesharkController.Device = value;
+                    }
+                    OnDeviceChanged();
+                }
+            }
+        }
+
+        public GamesharkController GamesharkController
+        {
+            get;
+        }
+
+        public IDictionary<ushort, BreakpointInfo> Breakpoints
+        {
+            get;
+        }
+
+        public void UnloadDevice()
+        {
+            var device = _currentDevice;
+            if (device != null)
+            {
+                device.Terminate();
+                _currentExternalMemory.Dispose();
+                _currentDevice = null;
+                OnDeviceUnloaded(new DeviceEventArgs(device));
+            }
+        }
+
+        public void LoadDevice(string romFilePath, string ramFilePath)
+        {
+            UnloadDevice();
+            _currentExternalMemory = new StreamedExternalMemory(File.Open(ramFilePath, FileMode.OpenOrCreate));
+            var cartridge = new EmulatedCartridge(File.ReadAllBytes(romFilePath), _currentExternalMemory);
+            _currentExternalMemory.SetBufferSize(cartridge.ExternalRamSize);
+            CurrentDevice = new GameBoy.GameBoy(cartridge, _ticker, !Settings.ForceOriginalGameBoy);
+            ApplyColorPalettes();
+            OnDeviceLoaded(new DeviceEventArgs(CurrentDevice));
+        }
+
+        protected virtual void OnDeviceChanged()
+        {
+            DeviceChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnDeviceLoaded(DeviceEventArgs e)
+        {
+            DeviceLoaded?.Invoke(this, e);
+        }
+
+        protected virtual void OnDeviceUnloaded(DeviceEventArgs e)
+        {
+            DeviceUnloaded?.Invoke(this, e);
+        }
+
+        private static Color ConvertColor(Color color)
+        {
+            return color;
+        }
+
+        private void ApplyColorPalettes()
+        {
+            if (CurrentDevice != null)
+            {
+                CurrentDevice.Gpu.Color0 = ConvertColor(Settings.GBColor0);
+                CurrentDevice.Gpu.Color1 = ConvertColor(Settings.GBColor1);
+                CurrentDevice.Gpu.Color2 = ConvertColor(Settings.GBColor2);
+                CurrentDevice.Gpu.Color3 = ConvertColor(Settings.GBColor3);
+            }
+        }
+
+        private void SettingsOnPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Contains("GBColor"))
+                ApplyColorPalettes();
+        }
+    }
+
+    public class GameBoyTicker : Container , IClock
+    {
+        public event EventHandler Tick;
+
+        private bool _tick;
+        private double _lastTickleTime;
+
+        public void Start()
+        {
+            _tick = true;
+        }
+
+        public void Stop()
+        {
+            _tick = false;
+        }
+
+        protected override void Update()
+        {
+            
+            if (_tick)
+            {
+                var time = this.Time.Current;
+
+                //Limit to 60HZ par second
+                if ((time - _lastTickleTime) > (1.0f / 60.0f) * 1000)
+                {
+                    Tick?.Invoke(this, EventArgs.Empty);
+                    _lastTickleTime = time;
+                }
+            }
         }
     }
 
@@ -106,21 +278,17 @@ namespace Emux.OpenTK.Visual
 
         public void RenderFrame(byte[] pixelData)
         {
-            var rgbaByte = new List<byte>();
-            for (int i = 0; i < pixelData.Length; i += 3)
-            {
-                byte R = pixelData[i];
-                byte G = pixelData[i + 1];
-                byte B = pixelData[i + 2];
-                byte A = 255;
+            var rawData = new byte[160 * 144 * sizeof(int)];
 
-                rgbaByte.Add(R);
-                rgbaByte.Add(G);
-                rgbaByte.Add(B);
-                rgbaByte.Add(A);
+            for (int i = 0, j = 0; j < pixelData.Length; i += 4, j += 3)
+            {
+                rawData[i] = pixelData[j];
+                rawData[i + 1] = pixelData[j + 1];
+                rawData[i + 2] = pixelData[j + 2];
+                rawData[i + 3] = 255;
             }
-            var image = Image.LoadPixelData<Rgba32>(rgbaByte.ToArray(), 160, 144);
-            
+
+            var image = Image.LoadPixelData<Rgba32>(rawData, 160, 144);
             Texture.SetData(new TextureUpload(image));
         }
     }
